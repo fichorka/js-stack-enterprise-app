3b2a3ad770eae780e3c1350506d3390b64eee4c8
fichorka <fichorka@gmail.com>
2020-09-26 01:29:52 +0200

    Initial commit

---
+++ .babelrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'entry'
+      }
+    ],
+    '@babel/preset-react',
+    '@babel/preset-typescript'
+  ]
+}
---
+++ webpack.dev.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/frontend/index.tsx',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dev_build')
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/frontend/html-templates/index.html'
+    })
+  ],
+  devServer: {
+    contentBase: './dev_build',
+    open: true
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.css']
+  },
+  module: {
+    rules: [
+      {
+        test: /.tsx?|jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+}
