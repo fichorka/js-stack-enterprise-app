3cb523961743a67f9e54e24159dc22e5a22425bd
fichorka <fichorka@gmail.com>
2020-09-26 15:11:16 +0200

    Create department entity

---
+++ src/backend/entities/department.ts
@@ -0,0 +1,51 @@
+import { ObjectId } from 'mongodb'
+
+const makeDepartment: MakeDepartment = function ({ departmentInfo }) {
+  const department: Department = {}
+
+  if (!departmentInfo.departmentName) {
+    throw new Error('departmentName does not exist.')
+  }
+
+  if (typeof departmentInfo.departmentName !== 'string') {
+    throw new Error('departmentName is not of string type.')
+  }
+
+  if (departmentInfo.departmentName.length > 20) {
+    throw new Error('departmenName is longer than 20 characters.')
+  }
+
+  department.departmentName = departmentInfo.departmentName
+
+  if (!departmentInfo.departmentLocation) {
+    throw new Error('departmentLocation does not exist.')
+  }
+
+  if (typeof departmentInfo.departmentLocation !== 'string') {
+    throw new Error('departmentLocation is not of string type.')
+  }
+
+  if (departmentInfo.departmentLocation.length > 20) {
+    throw new Error('departmentLocation is longer than 20 characters.')
+  }
+
+  department.departmentLocation = departmentInfo.departmentLocation
+
+  if (departmentInfo._id) department._id = departmentInfo._id
+
+  return department
+}
+
+export { makeDepartment }
+
+type MakeDepartment = ({
+  departmentInfo
+}: {
+  departmentInfo: Department
+}) => Department
+
+interface Department {
+  _id?: ObjectId
+  departmentName?: string
+  departmentLocation?: string
+}
