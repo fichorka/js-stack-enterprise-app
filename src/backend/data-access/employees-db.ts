a1b310047ef440ced27d3d2fb89a7969640b9b67
fichorka <fichorka@gmail.com>
2020-09-28 16:45:38 +0200

    Create employees-db

---
+++ src/backend/data-access/employees-db.ts
@@ -0,0 +1,74 @@
+import { ObjectId } from 'mongodb'
+import { Employee } from '../entities'
+import { MakeDb } from './type'
+
+const makeEmployeesDb: MakeEmployeesDb = function ({ makeDb }) {
+  const findOne: FindOne = async function (id) {
+    const db = await makeDb()
+    const employee = await db.collection('employees').findOne({ _id: id })
+
+    return employee || null
+  }
+
+  const findAll: FindAll = async function ({ limit, skip }) {
+    const db = await makeDb()
+    const employeeList: Employee[] | [] = await db
+      .collection('employees')
+      .find()
+      .skip(skip)
+      .limit(limit)
+      .toArray()
+
+    return employeeList
+  }
+
+  const insertOne: InsertOne = async function (employee) {
+    const db = await makeDb()
+    const insertOperation = await db.collection('employees').insertOne(employee)
+
+    return insertOperation?.result?.ok ? employee : null
+  }
+
+  const updateOne: UpdateOne = async function (employee) {
+    const db = await makeDb()
+    const insertOperation = await db
+      .collection('employees')
+      .updateOne({ _id: employee._id }, { $set: employee })
+
+    return insertOperation?.result?.ok ? employee : null
+  }
+
+  return {
+    findOne,
+    findAll,
+    insertOne,
+    updateOne
+  }
+}
+
+export { makeEmployeesDb }
+
+type MakeEmployeesDb = ({
+  makeDb
+}: {
+  makeDb: MakeDb
+}) => {
+  findOne: FindOne
+  findAll: FindAll
+  insertOne: InsertOne
+  updateOne: UpdateOne
+}
+
+type FindOne = (id: ObjectId) => Promise<Employee | null>
+
+type FindAll = ({
+  limit,
+  skip
+}: {
+  limit: number
+  skip: number
+}) => Promise<Employee[] | []>
+
+type InsertOne = (employee: Employee) => Promise<Employee | null>
+
+type UpdateOne = (employee: Employee) => Promise<Employee | null>
